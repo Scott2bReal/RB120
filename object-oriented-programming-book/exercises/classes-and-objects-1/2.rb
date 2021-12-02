66b2623ded084bc2cc1406a8f4a7db917736d577
Scott Hoecker <hoeckers@gmail.com>
2021-12-02 13:30:52 -0600

    Add question as comment to exercise files

--- object-oriented-programming-book/exercises/classes-and-objects-1/2.rb
+++ object-oriented-programming-book/exercises/classes-and-objects-1/2.rb
@@ -1,3 +1,11 @@
+=begin
+
+Add an accessor method to your MyCar class to change and view the color of your
+car. Then add an accessor method that allows you to view, but not modify, the
+year of your car.
+
+=end
+
 class MyCar
   attr_accessor :color
   attr_reader :year
--- object-oriented-programming-book/exercises/classes-and-objects-1/3.rb
+++ object-oriented-programming-book/exercises/classes-and-objects-1/3.rb
@@ -1,3 +1,11 @@
+=begin
+
+You want to create a nice interface that allows you to accurately describe the
+action you want your program to perform. Create a method called spray_paint that
+can be called on an object and will modify the color of the car.
+
+=end
+
 class MyCar
   attr_accessor :color, :speed
   attr_reader :year, :model
