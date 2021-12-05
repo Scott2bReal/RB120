670d77e47ef5fcd9b576b8dc7c4a64bec51d3be2
Scott Hoecker <hoeckers@gmail.com>
2021-12-05 09:54:10 -0600

    Finish second set of oop basic exercises

---
+++ oop-basic-exercises/classes-and-objects-2/1_generic_greeting_1.rb
@@ -0,0 +1,17 @@
+# Modify the following code so that Hello! I'm a cat! is printed when
+# Cat.generic_greeting is invoked.
+#
+# class Cat
+# end
+#
+# Cat.generic_greeting
+#
+class Cat
+  def self.generic_greeting
+    puts "Hello! I'm a cat!"
+  end
+end
+
+kitty = Cat.new
+kitty.class.generic_greeting
+Cat.generic_greeting
