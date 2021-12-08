aa4f862f282b8e5b6ac78e31a70033c88a3d4027
Scott Hoecker <hoeckers@gmail.com>
2021-12-08 13:59:12 -0600

    Study

--- rock-paper-scissors/rps_score_draft.rb
+++ rock-paper-scissors/rps_score_draft.rb
@@ -1,6 +1,4 @@
 class Move
-  include Comparable # Decided to use Comparable after encountering it in an exercise
-
   attr_reader :value
 
   VALUES = ['rock', 'paper', 'scissors']
@@ -21,8 +19,16 @@ class Move
     @value == 'scissors'
   end
 
-  def <=>(other_move) # Refactor of < and > methods for simplicity
-    move <=> other_move
+  def >(other_move)
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
   end
 
   def to_s
@@ -30,32 +36,16 @@ class Move
   end
 end
 
-# In this draft will try giving the Player class a score attribute
-
 class Player
-  attr_accessor :move, :name, :score, :winner
+  attr_accessor :move, :name, :score
 
-  def initialize
+  def initialize(player_type=:human)
+    @player_type = player_type
     @move = nil
     @name = set_name
     @score = 0
-    @winner = false
-  end
-
-  def winner?
-    self.winner
-  end
-
-  def wins!
-    self.winner = true
-    self.score += 1
-  end
-
-  def to_s
-    self.name
   end
 end
-
 
 class Human < Player
   def set_name
@@ -95,38 +85,28 @@ class Computer < Player
   end
 end
 
-# Give Player a @winner
-# Change display_winner to determine winner
-# Make replacement display_winner method
-
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
     @human = Human.new
-    @computer = Computer.new  
+    @computer = Computer.new(:computer)
   end
 
   def display_welcome_message
     puts "Welcome to Rock, Paper, Scissors!"
   end
 
-  def display_moves # Extracted this method from display_winner
-    puts "\n"
+  def display_moves
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
   end
 
-  def determine_winner!
-    human.move > computer.move ? human.wins! : computer.wins!
-  end
-
   def display_winner
-    puts "\n"
-    if human.winner?
-      puts "#{human} wins!"
-    elsif computer.winner?
-      puts "#{computer} wins!"
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
     else
       puts "It's a tie!"
     end
@@ -134,10 +114,6 @@ class RPSGame
 
   def display_goodbye_message
     puts "Thanks for playing Rock, Paper, Scissors. Goodbye!"
-  end
-
-  def display_score
-    puts "The score is Human: #{human.score}, Computer: #{computer.score}"
   end
 
   def play_again?
@@ -153,33 +129,15 @@ class RPSGame
     false
   end
 
-  def ultimate_winner?
-    return true if human.score == 3 || computer.score == 3
-  end
-
-  def reset_scores!
-    human.score = 0
-    computer.score = 0
-  end
-
-  def best_of_three 
-    loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
-      display_score
-      break if ultimate_winner?
-    end
-  end
-
   def play
     display_welcome_message
     loop do
-      self.best_of_three
+      human.choose
+      computer.choose
+      display_moves # New in this draft
+      display_winner
       break unless play_again?
     end
-
     display_goodbye_message
   end
 end
