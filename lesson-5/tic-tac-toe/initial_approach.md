67802f18bc36be605de35c411ad85cafe667b7c6
Scott Hoecker <hoeckers@gmail.com>
2021-12-18 16:26:02 -0600

    Start lesson 5

---
+++ lesson-5/tic-tac-toe/initial_approach.md
@@ -0,0 +1,25 @@
+# Tic Tac Toe
+
+## Short description
+
+Tic Tac Toe is a two player board game played on a 3x3 grid. Players take turns
+marking a square. The first player to mark 3 squares in a row wins.
+
+## Nouns & Verbs
+
+### Nouns
+
+Board, Player, Piece, Square, Grid
+
+### Verbs
+
+play, mark, win
+
+### Organization
+
+Board
+Square
+Player
+  - mark
+  - play
+  - win
---
+++ lesson-5/tic-tac-toe/initial_spike.rb
@@ -0,0 +1,71 @@
+class Board
+  def initialize
+
+  end
+end
+
+class Square
+  def initialize
+    # maybe a "status" to keep track of this square's mark?
+  end
+end
+
+class Player
+  def initialize
+    # maybe a "marker" to keep track of this player's symbol (ie, 'X' or 'O')
+  end
+
+  def mark
+  end
+end
+
+class TTTGame
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "  board.get_square_at(1)   |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts ""
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      display_board(board)
+      break
+      first_player_moves
+      break if someone_won? || board_full?
+
+      second_player_moves
+      break if someone_won? || board_full?
+    end
+    #display_result
+    display_goodbye_message
+  end
+end
+
+game = TTTGame.new
+game.play
