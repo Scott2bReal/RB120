5017a3f000506c0a2805d30997fb623f04c37203
Scott Hoecker <hoeckers@gmail.com>
2021-12-18 21:53:14 -0600

    Refactor winning_marker

--- lesson-5/tic-tac-toe/ttt.rb
+++ lesson-5/tic-tac-toe/ttt.rb
@@ -15,49 +15,13 @@ class Board
     set_square_at(key, marker)
   end
 
+  def [](key)
+    @squares[key]
+  end
+
   def initialize
     @squares = {}
     reset
-  end
-
-  def set_square_at(key, marker)
-    @squares[key].marker = marker
-  end
-
-  def unmarked_keys
-    @squares.keys.select { |key| @squares[key].unmarked? }
-  end
-
-  def full?
-    unmarked_keys.empty?
-  end
-
-  def someone_won?
-    !!winning_marker
-  end
-
-  def count_human_marker(squares)
-    squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
-  end
-
-  def count_computer_marker(squares)
-    squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
-  end
-
-  # returns winning marker or nil
-  def winning_marker
-    WINNING_LINES.each do |line|
-      if count_human_marker(@squares.values_at(*line)) == 3
-        return TTTGame::HUMAN_MARKER
-      elsif count_computer_marker(@squares.values_at(*line)) == 3
-        return TTTGame::COMPUTER_MARKER
-      end
-    end
-    nil
-  end
-
-  def reset
-    (1..9).each { |key| @squares[key] = Square.new }
   end
 
   def draw
@@ -76,6 +40,50 @@ class Board
         |     |
     
     MSG
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
+  end
+
+  def full?
+    unmarked_keys.empty?
+  end
+
+  def someone_won?
+    !!winning_marker
+  end
+
+  # returns winning marker or nil
+  def winning_marker
+    WINNING_LINES.each do |line|
+      markers = line.map { |key| @squares[key].marker }
+
+      next if any_unmarked?(markers)
+
+      return markers.first if winning_line?(markers)
+    end
+
+    nil
+  end
+
+  def reset
+    (1..9).each { |key| @squares[key] = Square.new }
+  end
+  private
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
+
+  def any_unmarked?(arr)
+    # unmarked_keys.include?(@squares[*arr])
+    arr.any?(Square::INITIAL_MARKER)
+  end
+
+  def winning_line?(line)
+    line.each { |marker| return true if line.count(marker) == 3 }
+    false
   end
 end
 
