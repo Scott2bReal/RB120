85fde7259b06e10de305794a76510b43c529cfdf
Scott Hoecker <hoeckers@gmail.com>
2021-12-22 10:50:23 -0600

    Initial deal works, added 10 to deck

--- lesson-5/twenty-one/twenty_one.rb
+++ lesson-5/twenty-one/twenty_one.rb
@@ -1,14 +1,13 @@
-# module Hand
-# end
+require 'pry'
 
 class Participant
   attr_reader :name
   attr_accessor :hand
 
-  def initialize(name)
+  def initialize
     # what would the 'data' or 'states' of a Player object entail?
     # maybe cards? a name?
-    @name = name
+    # @name = name
     @hand = Hand.new
   end
 
@@ -25,44 +24,46 @@ class Player < Participant
   def show_hand
     # show all cards in hand
     # probably total as well
+    puts hand.cards
   end
 end
 
 class Dealer < Participant
   def show_hand
     # only show one card
+    puts hand.cards
   end
 end
 
 class Deck
   CARD_VALUES = {
-    '2' => 2,
-    '3' => 3,
-    '4' => 4,
-    '5' => 5,
-    '6' => 6,
-    '7' => 7,
-    '8' => 8,
-    '9' => 9,
-    'J' => 10,
-    'Q' => 10,
-    'K' => 10,
-    'A' => 11
+    ' 2' => 2,
+    ' 3' => 3,
+    ' 4' => 4,
+    ' 5' => 5,
+    ' 6' => 6,
+    ' 7' => 7,
+    ' 8' => 8,
+    ' 9' => 9,
+    '10' => 10,
+    ' J' => 10,
+    ' Q' => 10,
+    ' K' => 10,
+    ' A' => 11
   }
 
   SUITS = %w(♠ ♥ ♣ ♦)
 
-  attr_reader :cards
+  attr_accessor :cards
 
   def initialize
     @cards = initialize_deck
-    # obviously, we need some data structure to keep track of cards
-    # array, hash, something else?
   end
 
-  def deal
+  def deal_card
     # does the dealer or the deck deal?
     # I'm thinking I want the deck to deal
+    cards.shuffle!.pop
   end
 
   private
@@ -102,6 +103,10 @@ class Hand # card will collaborate
     calculate_aces if total > MAX_POINTS
   end
 
+  def to_s
+    @cards
+  end
+
   private
 
   def calculate_aces
@@ -132,7 +137,7 @@ class Card
     *-----*
     |#{suit}    |
     |     |
-    |  #{name}  |
+    | #{name}  |
     |     |
     |   #{suit} |
     *-----*
@@ -141,15 +146,44 @@ class Card
 end
 
 class Game
+  INITIAL_DEAL = 2
+  NUMBER_OF_PLAYERS = 2
+
+  attr_reader :deck, :player, :dealer
+
+  def initialize
+    # Needs a dealer, player, deck, current_player
+    @deck = Deck.new
+    @player = Player.new
+    @dealer = Dealer.new
+    @current_player = @player
+  end
+
   def start
-    deal_cards
+    initial_deal
     show_initial_cards
-    player_turn
-    dealer_turn
-    show_result
+    # player_turn
+    # dealer_turn
+    # show_result
+  end
+
+  private
+
+  def initial_deal
+    [player, dealer].each do |participant|
+      INITIAL_DEAL.times do
+        participant.hand.cards << deck.deal_card
+      end
+    end
+  end
+
+  def show_initial_cards
+    [player, dealer].each(&:show_hand)
+    puts "Deck: "
+    puts deck.cards
   end
 end
 
-# Game.new.start
-deck = Deck.new
-puts deck.cards
+Game.new.start
+# deck = Deck.new
+# puts deck.cards
