dee30ba7467041caa941d37273e8d04bb2da6113
Scott Hoecker <hoeckers@gmail.com>
2021-12-25 10:15:41 -0600

    Work on medium 1 oop exercises

--- oop-basic-exercises/medium-1/4_circular_queue.rb
+++ oop-basic-exercises/medium-1/4_circular_queue.rb
@@ -11,3 +11,96 @@
 
 # Assuming we have a circular queue with room for 3 objects, the circular queue
 # looks and acts like this:
+#
+# P1 	P2 	P3 	Comments
+#             All positions are initially empty
+# 1 			    Add 1 to the queue
+# 1 	2 		  Add 2 to the queue
+#     2 		  Remove oldest item from the queue (1)
+#    	2 	3   Add 3 to the queue
+# 4 	2 	3 	Add 4 to the queue, queue is now full
+# 4 	  	3 	Remove oldest item from the queue (2)
+# 4 	5 	3 	Add 5 to the queue, queue is full again
+# 4 	5 	6 	Add 6 to the queue, replaces oldest element (3)
+# 7 	5 	6 	Add 7 to the queue, replaces oldest element (4)
+# 7 		  6 	Remove oldest item from the queue (5)
+# 7 			    Remove oldest item from the queue (6)
+#             Remove oldest item from the queue (7)
+#             Remove non-existent item from the queue (nil)
+
+# Your task is to write a CircularQueue class that implements a circular queue
+# for arbitrary objects. The class should obtain the buffer size with an
+# argument provided to CircularQueue::new, and should provide the following
+# methods:
+
+# * enqueue to add an object to the queue
+# * dequeue to remove (and return) the oldest object in the queue. It should
+# return nil if the queue is empty.
+
+# You may assume that none of the values stored in the queue are nil (however,
+# nil may be used to designate empty spots in the buffer).
+
+# Nailed the further exploration with initial solution yesssss
+
+class CircularQueue
+  def initialize(size)
+    @size = size
+    @queue = []
+  end
+
+  def enqueue(item)
+    dequeue if queue.size == size
+    queue << item
+  end
+
+  def dequeue
+    queue.shift
+  end
+
+  private
+
+  attr_reader :size
+  attr_accessor :queue
+end
+
+# Examples:
+queue = CircularQueue.new(3)
+puts queue.dequeue.nil?
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue.nil?
+
+queue = CircularQueue.new(4)
+puts queue.dequeue.nil?
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 4
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue.nil?
+
+# The above code should print true 15 times
---
+++ oop-basic-exercises/medium-1/circular_queue.md
@@ -0,0 +1,8 @@
+Algorithm:
+  - Queue starts as array of size size, each element is `nil`
+  - Set idx to 0
+  - If dequeue is called, return `nil`
+  - If enqueue is called, element at idx is now the passed in item, unless
+    element at that item is something other than nil
+  
+  
